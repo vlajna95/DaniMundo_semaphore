4e2df22bb7b197ecd133d824dee8ce3035b5f134
Danijela Popović <vlajna95@gmail.com>
2022-02-01 22:15:34 +0100

    Changed the project settings to include a list of available languages

--- DaniMundo/settings.py
+++ DaniMundo/settings.py
@@ -100,7 +100,12 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/4.0/topics/i18n/
 
-LANGUAGE_CODE = "en-us"
+LANGUAGES = [
+	("en-us", "English (United States)"),
+	("es", "Español (España)"),
+	("sr", "Српски (ћирилица)"),
+]
+LANGUAGE_CODE = "sr"
 TIME_ZONE = "Europe/Belgrade"
 USE_I18N = True
 USE_L10N = True
