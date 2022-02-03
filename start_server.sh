8267e60d99d78604ba7a70d8a1bf22232865c5bf
Danijela Popović <eternal.romania@gmail.com>
2022-02-03 11:31:03 +0100

    Switched back to the old folder structure

--- start_server.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-# start_server.sh
-
-if [ -n "$DJANGO_SUPERUSER_USERNAME" ] && [ -n "$DJANGO_SUPERUSER_EMAIL" ] && [ -n "$DJANGO_SUPERUSER_PASSWORD" ] ; then
-    (cd DaniMundo; python3 manage.py createsuperuser --no-input)
-fi
-(cd DaniMundo; gunicorn DaniMundo.wsgi --user www-data --bind 0.0.0.0:8010 --workers 3) &
-nginx -g "daemon off;"
